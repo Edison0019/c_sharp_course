d0b65258c4c6cce64bf89ad540c7c2bf05993719
edison0019 <edison_0019@hotmail.com>
2021-03-04 00:17:38 -0400

    new project with variable interactions

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+
+namespace HelloWorld
+{
+    //a simple program to print hello world in this
+    //awesome programming languange
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+            Console.Read();
+        }
+    }
+}
---
+++ helloWorldAgain/helloWorldAgain/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace helloWorldAgain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName = "";
+            int userAge = 0;
+            int currentYear = 0;
+            Console.Write("Please enter your name: ");
+            userName = Console.ReadLine();
+            Console.Write("Please enter your age: ");
+            userAge = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the current year: ");
+            currentYear = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(
+                "Hello world! my name is {0} my age is {1} in the year {2}",
+                userName,
+                userAge,
+                currentYear
+                );
+        }
+    }
+}
