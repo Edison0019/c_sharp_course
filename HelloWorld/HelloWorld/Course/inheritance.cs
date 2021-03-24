de134f7c1c7e137bab0cb2c04504a20fec6c6c33
edison0019 <edison_0019@hotmail.com>
2021-03-23 22:26:18 -0400

    Added inheritance lesson

---
+++ HelloWorld/HelloWorld/Course/inheritance.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld.Course
+{
+    class Member
+    {
+        protected int annualFee;
+        private string name;
+        private int memberID;
+        private int memberSince;
+
+
+        public Member()
+        {
+            Console.WriteLine("Parent constructor with no parameter");
+        }
+
+        public Member(string pNamen, int pMemberID, int pMemberSince)
+        {
+            Console.WriteLine("Parent constructor with three parameters");
+
+            name = pNamen; 
+            memberID = pMemberID; 
+            memberSince = pMemberSince;
+
+        }
+        public override string ToString()
+        {
+            return "\nName: " + name + "\nMember: " + memberID +
+                "\nMember since: " + memberSince + "\nTotal annual fee: " +
+                annualFee;
+        }
+
+    }
+
+    class NormalMember : Member
+    {
+        public NormalMember()
+        {
+            Console.WriteLine("Child constructor");
+        }
+        //this approach allows to instantiate the parent class
+        //using a constructor from the child class
+        public NormalMember(string remarks, string name,int memberID, int memberSince) : base(
+            name,memberID,memberSince)
+        {
+            Console.WriteLine("Child constructor with 4 parameter");
+            Console.WriteLine("Remarks: {0}",remarks);
+        }
+        public void calculateFee()
+        {
+            //since the field already exists in the parent class
+            //we do not need to declare it twice in the child class
+            annualFee = 100 + 12 * 30;
+        }
+    }
+
+    class VIPMember : Member
+    {
+        public VIPMember(string name, int memberID , int memberSince):base(
+                name,memberID,memberSince)
+        {
+            Console.WriteLine("Child class with three parameters");
+        }
+        public void CalculateAnualFee()
+        {
+            annualFee = 1200;
+        }
+
+    }
+}
--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HelloWorld.Course;
 
 namespace HelloWorld
 {
@@ -17,12 +18,17 @@ namespace HelloWorld
             //the data type of the object
             //the below is an example of how to import values and
             //create object from classes in files
-            
+
             /*
                 - we will use this namespace to test the values and the code of this course
              */
-            var program = new switchStatement.Program();
-            program.switchStatement();
+            NormalMember Javier = new NormalMember("Be humble","Javier", 2, 2018);
+            VIPMember Edison = new VIPMember("Edison",1,2020);
+            Javier.calculateFee();
+            Edison.CalculateAnualFee();
+
+            Console.WriteLine(Javier.ToString());
+            Console.WriteLine(Edison.ToString());
         }
     }
 }
